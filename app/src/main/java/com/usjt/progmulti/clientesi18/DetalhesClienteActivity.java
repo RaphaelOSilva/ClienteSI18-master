d9e3971780cf7bbf5bc6e160e0945589b2a2b8e2
arqdsis <arqdsis@mocli10234.animaedu.intranet>
2018-03-14 18:05:11 -0300

    Initial commit

---
+++ app/src/main/java/com/usjt/progmulti/clientesi18/DetalhesClienteActivity.java
@@ -0,0 +1,23 @@
+package com.usjt.progmulti.clientesi18;
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+import com.usjt.progmulti.clientesi18.ListaClientesActivity;
+
+/**
+ * Created by arqdsis on 13/03/2018.
+ */
+
+public class DetalhesClienteActivity extends Activity {
+    TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalhe_cliente);
+        TextView nome = (TextView)findViewById(R.id.nome_selecionado);
+        Intent intent = getIntent();
+        nome.setText(intent.getStringExtra(ListaClientesActivity.NOME)); }
+}
